3c898325af6f941ef268315d020be72581791481
MoNasr2022 <efmnt2020@gmail.com>
2022-08-24 23:53:10 -0400

    Update Main.java

--- src/Chap_2/Main.java
+++ src/Chap_2/Main.java
@@ -474,7 +474,7 @@ public class Main {
 //
 //
 
-
+/*
 public class Main {
     public static void main(String[] args) {
        Integer[] nums = new Integer[]{1,5,0,3,5};
@@ -521,4 +521,32 @@ public class Main {
 
 
     }
+}
+
+ */
+
+//===================
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    int[] nums =new int[] {0,1,4,6,7,10};
+       int diff = 3;
+
+        int count = 0;
+        for( int i =0; i <nums.length; i++){
+            for(int j = i +1; j<nums.length; j++){
+                for (int k = j+1; k<nums.length; k++){
+                    if (nums[j] - nums[i] == diff && nums[k] - nums[j] == diff) count++;
+                }
+            }
+
+        }
+        System.out.println(count);
+
+
+
+    }
+
 }
